04aaea5d1aa1f3100984719007431f8042b163e8
billsun9 <53660703+billsun9@users.noreply.github.com>
2020-09-01 21:56:59 -0400

    entire project

---
+++ train.py
@@ -0,0 +1,50 @@
+# define the standalone discriminator model
+
+from keras.optimizers import Adam
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import Reshape
+from keras.layers import Flatten
+from keras.layers import Conv2D
+from keras.layers import Conv2DTranspose
+from keras.layers import LeakyReLU
+from keras.layers import Dropout
+from keras.layers import Embedding
+from keras.layers import Concatenate
+
+def define_discriminator(in_shape=(28,28,1), n_classes=10):
+	# label input
+	in_label = Input(shape=(1,))
+	# embedding for categorical input
+	li = Embedding(n_classes, 50)(in_label)
+	# scale up to image dimensions with linear activation
+	n_nodes = in_shape[0] * in_shape[1]
+	li = Dense(n_nodes)(li)
+	# reshape to additional channel
+	li = Reshape((in_shape[0], in_shape[1], 1))(li)
+	# image input
+	in_image = Input(shape=in_shape)
+	# concat label as a channel
+	merge = Concatenate()([in_image, li])
+	# downsample
+	fe = Conv2D(128, (3,3), strides=(2,2), padding='same')(merge)
+	fe = LeakyReLU(alpha=0.2)(fe)
+	# downsample
+	fe = Conv2D(128, (3,3), strides=(2,2), padding='same')(fe)
+	fe = LeakyReLU(alpha=0.2)(fe)
+	# flatten feature maps
+	fe = Flatten()(fe)
+	# dropout
+	fe = Dropout(0.4)(fe)
+	# output
+	out_layer = Dense(1, activation='sigmoid')(fe)
+	# define model
+	model = Model([in_image, in_label], out_layer)
+	# compile model
+	opt = Adam(lr=0.0002, beta_1=0.5)
+	model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+	return model
+
+model = define_discriminator()
+model.summary()
